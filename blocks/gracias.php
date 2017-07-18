ecc95d78073d9803d74ae3fc29e60b09a0542837
Carlos Rubio Mac BM <carlosblondsk8@gmail.com>
2017-07-18 15:02:34 -0500

    Inicia repo

---
+++ blocks/gracias.php
@@ -0,0 +1,19 @@
+<section class="hero-gracias">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<img src="img/logo_security_vision.png" data-src="img/logo_security_vision.png | img/logo_security_vision@2x.png" alt="" class="lazyImg center-block">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-8 col-lg-8 col-sm-12 col-xs-12">
+				<article>
+					<h1>Gracias por escribirnos</h1>
+					<p>Pronto estarémos en contacto.</p>
+					</article>
+				</div>
+			</div>
+		</div>
+</section>
+	<button type="submit" class="btn btn-sub center-block fade" id="burguer-button">Asesoría Personalizada</button>
+	<button type="submit" class="btn btn-sub center-block fade" id="burguer-button2">Asesoría Personalizada</button>
