aa973c1ccf84438939b2a85e5e187ba7b69a98a7
Carlos Rubio Mac BM <carlosblondsk8@gmail.com>
2017-07-19 10:07:33 -0500

    description and steps

--- blocks/home.php
+++ blocks/home.php
@@ -5,6 +5,7 @@
 				<div class="hero-program-name">
 					<h1>AMP FOR CEO</h1>
 					<span>Advanced Management Program</span>
+					<div class="animated-line"></div>
 				</div>
 				<div class="hero-copy">
 					<h2>Visión Estratégica</h2>
@@ -27,8 +28,47 @@
 		</div>
 	</div>
 </section>
+<section class="tittle-description">
+	<h1>AMP FOR CEO</h1>
+	<span>Advanced Management Program</span>
+	<div class="animated-line"></div>
+</section>
+<section class="description">
+	<div class="paralax">
+		<article>
+			<p>
+				El Programa de Alta Gerencia y Dirección, Advanced Management Program, tiene como objetivo brindar al estudiante un conocimiento integral sobre <br/>las organizaciones, desarrollar habilidades, conocimientos y aptitudes, de tal forma que la persona esté preparada teórica y técnicamente<br/> para enfrentarse a la dirección de las empresas, en el ámbito estatal o privado.
+			</p>
+			<button type="submit" class="btn center-block btn-cta" id="burguer-button">
+				MÁS INFORMACIÓN
+			</button>
+		</article>
+	</div>
+</section>
+
+<section class="tittle-description">
+	<h1>METODOLOGÍA</h1>
+	<div class="animated-line"></div>
+</section>
+
+<section class="metodology">
+	<div class="content-steps">
+		<div class="step">
+			<h4>Evaluación 360</h4>
+		</div>
+		<div class="step">
+			<h4>Plan de desarrollo</h4>
+		</div>
+		<div class="step">
+			<h4>Mentoría Personalizada</h4>
+		</div>
+		<div class="step">
+			<h4>
+				Portafolio de aprendizaje <br/>consignando competencias adquiridas
+			</h4>
+		</div>
+	</div>
+</section>
 
 
-
-<button type="submit" class="btn center-block" id="burguer-button">Asesoría Personalizada</button>
 <button type="submit" class="btn center-block" id="burguer-button2">Asesoría Personalizada</button>
